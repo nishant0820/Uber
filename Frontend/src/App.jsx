0d1c140c092753f1ba122c101b25e1e9412ff352
Nishant Gupta <gupta.nishant08042004@gmail.com>
2025-07-13 09:57:39 +0530

    captain logout endpoint complete

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -10,6 +10,7 @@ import UserProtectWrapper from './pages/UserProtectWrapper'
 import UserLogout from './pages/UserLogout'
 import CaptainHome from './pages/CaptainHome'
 import CaptainProtectWrapper from './pages/CaptainProtectWrapper'
+import CaptainLogout from './pages/CaptainLogout'
 
 const App = () => {
   return (
@@ -28,6 +29,9 @@ const App = () => {
         } />
         <Route path='/captain-home' element={
           <CaptainProtectWrapper><CaptainHome /></CaptainProtectWrapper>
+        } />
+        <Route path='/captain/logout' element={
+          <CaptainProtectWrapper><CaptainLogout /></CaptainProtectWrapper>
         } />
       </Routes>
     </div>
